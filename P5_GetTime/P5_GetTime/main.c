227daa6df6ff76f3c131ab3a80688a1046fa4567
yuanyuan100 <yuanyuan_pyy@163.com>
2017-07-27 23:19:20 +0800

    [add]检索例子文件

---
+++ P5_GetTime/P5_GetTime/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  P5_GetTime
+//
+//  Created by Mr.PYY on 17/7/27.
+//  Copyright © 2017年 Mr.PYY. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
